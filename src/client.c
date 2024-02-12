329a3c0683e0a9cdea6fccf8e3de5be54d66edbe
Jamason Davis <jpdavis2471@gmail.com>
2024-02-12 08:58:55 -0600

    This scares me lol

--- src/client.c
+++ src/client.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+
+void createSever() {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    bind(sock, sockaddr)
+}
 
 int main(int argc, char* argv[]) {
     printf("Hello World!\n");
